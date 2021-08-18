87e9926971b42386b3057a940f9e3175cacb1f2a
ceren.tarhan <ceren.tarhan.15133@ozu.edu.tr>
2021-08-18 11:38:00 +0300

    third commit

---
+++ TourVisio.Hotel.Client/Models/ReservationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TourVisio.WebService.Adapter.Models;
+using TourVisio.WebService.Adapter.ServiceModels.BookingModels;
+
+namespace TourVisio.Hotel.Client.Models
+{
+    public class ReservationModel
+    {
+        public IEnumerable<mdlOfferDetail> offer;
+
+        public string ErrorMsg { get; set; }
+        public TransactionResponse TransactionResponse { get;  set; }
+        public IEnumerable<RoomingBlock> Rooms { get; set; }
+        public IEnumerable<mdlNationalityItem> Nationalities { get; set; }
+    }
+}
--- TourVisio.Hotel.Client/wwwroot/js/index.js
+++ TourVisio.Hotel.Client/wwwroot/js/index.js
@@ -1,8 +1,6 @@
-﻿
+﻿$(document).ready(function() {
 
-$(document).ready(function () {
-
-    $("[name='Child1']").change(function () {
+    $("[name='Child1']").change(function() {
 
         var value = $(this).val();
         for (i = 1; i <= 4; i++) {
@@ -10,28 +8,28 @@ $(document).ready(function () {
         }
     });
 
-    $("[name='Child2']").change(function () {
+    $("[name='Child2']").change(function() {
         var value = $(this).val();
         for (i = 1; i <= 4; i++) {
             $("[name='ChildAge2" + i + "']").attr("disabled", value >= i ? false : true);
         }
     });
 
-    $("[name='Child3']").change(function () {
+    $("[name='Child3']").change(function() {
         var value = $(this).val();
         for (i = 1; i <= 4; i++) {
             $("[name='ChildAge3" + i + "']").attr("disabled", value >= i ? false : true);
         }
     });
 
-    $("[name='Child4']").change(function () {
+    $("[name='Child4']").change(function() {
         var value = $(this).val();
         for (i = 1; i <= 4; i++) {
             $("[name='ChildAge4" + i + "']").attr("disabled", value >= i ? false : true);
         }
     });
 
-    $("[name='Room']").change(function () {
+    $("[name='Room']").change(function() {
         var value = $(this).val();
         for (i = 1; i <= 4; i++) {
             $("[name='Adult" + i + "']").attr("disabled", value >= i ? false : true);
@@ -51,9 +49,9 @@ $(document).ready(function () {
     });
 
     $("[name='Location']").autocomplete({
-        source: function (request, response) {
-            $.getJSON("/Home/Location", request, function (data) {
-                response($.map(data, function (item) {
+        source: function(request, response) {
+            $.getJSON("/Home/Location", request, function(data) {
+                response($.map(data, function(item) {
                     var name = "";
                     var id = "";
 
@@ -110,15 +108,18 @@ $(document).ready(function () {
                                 name = village + " " + town + " " + hotel + " " + city + " " + country;
                             }
                             break;
+                        default:
+                            break;
                     }
+
                     return {
                         label: name,
                         value: id
-                    }
-                }))
+                    };
+                }));
             });
         },
-        select: function (event, ui) {
+        select: function(event, ui) {
             $("[name='Location']").val(ui.item.label);
             $("[name='LocationId']").val(ui.item.value);
             $("[name='LocationType']").val(ui.item.value);
@@ -126,66 +127,141 @@ $(document).ready(function () {
         }
     });
 
-        $("[name='btnPost']").click(function () {
-            var searchForm = new Object();
-            searchForm.Location = $("[name='Location']").val();
-            searchForm.LocationId = $("[name='LocationId']").val();
-            searchForm.LocationType = parseInt($("[name='LocationType']").val());
-            searchForm.CheckInDate = $("[name='CheckInDate']").val();
-            searchForm.CheckOutDate = $("[name='CheckOutDate']").val();
-            searchForm.Nationality = $("[name='Nationality']").val();
-            searchForm.Currency = $("[name='Currency']").val();
-            searchForm.Room =  parseInt($("[name='Room']").val());
-     
-            searchForm.Adult1 = $("[name='Adult1']").is(":disabled") ? 0 : parseInt($("[name='Adult1']").val());
-            searchForm.Child1 =  parseInt($("[name='Child1']").val());
-            searchForm.ChildAge11 = $("[name='ChildAge11']").is(":disabled") ? 0 : parseInt($("[name='ChildAge11']").val());
-            searchForm.ChildAge12 = $("[name='ChildAge12']").is(":disabled") ? 0 : parseInt($("[name='ChildAge12']").val());
-            searchForm.ChildAge13 = $("[name='ChildAge13']").is(":disabled") ? 0 : parseInt($("[name='ChildAge13']").val());
-            searchForm.ChildAge14 = $("[name='ChildAge14']").is(":disabled") ? 0 : parseInt($("[name='ChildAge14']").val());
+    $("[name='btnPost']").click(function() {
+        var searchForm = new Object();
+        searchForm.Location = $("[name='Location']").val();
+        searchForm.LocationId = $("[name='LocationId']").val();
+        searchForm.LocationType = parseInt($("[name='LocationType']").val());
+        searchForm.CheckInDate = $("[name='CheckInDate']").val();
+        searchForm.CheckOutDate = $("[name='CheckOutDate']").val();
+        searchForm.Nationality = $("[name='Nationality']").val();
+        searchForm.Currency = $("[name='Currency']").val();
+        searchForm.Room = parseInt($("[name='Room']").val());
 
-            searchForm.Adult2 = $("[name='Adult2']").is(":disabled") ? 0 : parseInt($("[name='Adult2']").val());
-            searchForm.Child2 = parseInt($("[name='Child2']").val());
-            searchForm.ChildAge21 = $("[name='ChildAge21']").is(":disabled") ? 0 : parseInt($("[name='ChildAge21']").val());
-            searchForm.ChildAge22 = $("[name='ChildAge22']").is(":disabled") ? 0 :parseInt($("[name='ChildAge22']").val());
-            searchForm.ChildAge23 = $("[name='ChildAge23']").is(":disabled") ? 0 : parseInt($("[name='ChildAge23']").val());
-            searchForm.ChildAge24 = $("[name='ChildAge24']").is(":disabled") ? 0 :parseInt($("[name='ChildAge24']").val());
+        searchForm.Adult1 = $("[name='Adult1']").is(":disabled") ? 0 : parseInt($("[name='Adult1']").val());
+        searchForm.Child1 = parseInt($("[name='Child1']").val());
+        searchForm.ChildAge11 = $("[name='ChildAge11']").is(":disabled") ? 0 : parseInt($("[name='ChildAge11']").val());
+        searchForm.ChildAge12 = $("[name='ChildAge12']").is(":disabled") ? 0 : parseInt($("[name='ChildAge12']").val());
+        searchForm.ChildAge13 = $("[name='ChildAge13']").is(":disabled") ? 0 : parseInt($("[name='ChildAge13']").val());
+        searchForm.ChildAge14 = $("[name='ChildAge14']").is(":disabled") ? 0 : parseInt($("[name='ChildAge14']").val());
 
-            searchForm.Adult3 = $("[name='Adult3']").is(":disabled") ? 0 : parseInt($("[name='Adult3']").val());
-            searchForm.Child3 = parseInt($("[name='Child3']").val());
-            searchForm.ChildAge31 = $("[name='ChildAge31']").is(":disabled") ? 0 : parseInt($("[name='ChildAge31']").val());
-            searchForm.ChildAge32 = $("[name='ChildAge32']").is(":disabled") ? 0 :parseInt($("[name='ChildAge32']").val());
-            searchForm.ChildAge33 = $("[name='ChildAge33']").is(":disabled") ? 0 :parseInt($("[name='ChildAge33']").val());
-            searchForm.ChildAge34 = $("[name='ChildAge34']").is(":disabled") ? 0 :parseInt($("[name='ChildAge34']").val());
+        searchForm.Adult2 = $("[name='Adult2']").is(":disabled") ? 0 : parseInt($("[name='Adult2']").val());
+        searchForm.Child2 = parseInt($("[name='Child2']").val());
+        searchForm.ChildAge21 = $("[name='ChildAge21']").is(":disabled") ? 0 : parseInt($("[name='ChildAge21']").val());
+        searchForm.ChildAge22 = $("[name='ChildAge22']").is(":disabled") ? 0 : parseInt($("[name='ChildAge22']").val());
+        searchForm.ChildAge23 = $("[name='ChildAge23']").is(":disabled") ? 0 : parseInt($("[name='ChildAge23']").val());
+        searchForm.ChildAge24 = $("[name='ChildAge24']").is(":disabled") ? 0 : parseInt($("[name='ChildAge24']").val());
 
-            searchForm.Adult4 = $("[name='Adult4']").is(":disabled") ? 0 : parseInt($("[name='Adult4']").val());
-            searchForm.Child4 = parseInt($("[name='Child4']").val());
-            searchForm.ChildAge41 = $("[name='ChildAge41']").is(":disabled") ? 0 : parseInt($("[name='ChildAge41']").val());
-            searchForm.ChildAge42 = $("[name='ChildAge42']").is(":disabled") ? 0 : parseInt($("[name='ChildAge42']").val());
-            searchForm.ChildAge43 = $("[name='ChildAge43']").is(":disabled") ? 0 : parseInt($("[name='ChildAge43']").val());
-            searchForm.ChildAge44 = $("[name='ChildAge44']").is(":disabled") ? 0 : parseInt($("[name='ChildAge44']").val());
+        searchForm.Adult3 = $("[name='Adult3']").is(":disabled") ? 0 : parseInt($("[name='Adult3']").val());
+        searchForm.Child3 = parseInt($("[name='Child3']").val());
+        searchForm.ChildAge31 = $("[name='ChildAge31']").is(":disabled") ? 0 : parseInt($("[name='ChildAge31']").val());
+        searchForm.ChildAge32 = $("[name='ChildAge32']").is(":disabled") ? 0 : parseInt($("[name='ChildAge32']").val());
+        searchForm.ChildAge33 = $("[name='ChildAge33']").is(":disabled") ? 0 : parseInt($("[name='ChildAge33']").val());
+        searchForm.ChildAge34 = $("[name='ChildAge34']").is(":disabled") ? 0 : parseInt($("[name='ChildAge34']").val());
+
+        searchForm.Adult4 = $("[name='Adult4']").is(":disabled") ? 0 : parseInt($("[name='Adult4']").val());
+        searchForm.Child4 = parseInt($("[name='Child4']").val());
+        searchForm.ChildAge41 = $("[name='ChildAge41']").is(":disabled") ? 0 : parseInt($("[name='ChildAge41']").val());
+        searchForm.ChildAge42 = $("[name='ChildAge42']").is(":disabled") ? 0 : parseInt($("[name='ChildAge42']").val());
+        searchForm.ChildAge43 = $("[name='ChildAge43']").is(":disabled") ? 0 : parseInt($("[name='ChildAge43']").val());
+        searchForm.ChildAge44 = $("[name='ChildAge44']").is(":disabled") ? 0 : parseInt($("[name='ChildAge44']").val());
 
 
-            
-            if (searchForm != null) {
-                $.ajax({
-                    type:"POST",
-                    url: "/Home/Search",
-                    data: JSON.stringify(searchForm),
-                    contentType:"application/json; charset=utf-8",
-                    dataType: "html",
-                    success: function (response) {
-                        $('#searchResult').html(response);
-                    },
-                    failure: function (response) {
-                        alert(response.responseText);
-                    },
-                    error: function (response) {
-                        alert(response.responseText);
-                    }
-                    
-                });
-               
-            }
-        });
+
+        if (searchForm != null) {
+            $.ajax({
+                type: "POST",
+                url: "/Home/Search",
+                data: JSON.stringify(searchForm),
+                contentType: "application/json; charset=utf-8",
+                dataType: "html",
+                success: function(response) {
+                    $('#searchResult').html(response);
+                },
+                failure: function(response) {
+                    alert(response.responseText);
+                },
+                error: function(response) {
+                    alert(response.responseText);
+                }
+            });
+        }
     });
+
+    $("#searchResult").on("click", ".hotel", function() {
+        var id = $(this).attr("id");
+        $("[name='offers" + id + "']").toggle();
+    });
+
+    $("#searchResult").on("click", ".book", function() {
+        var id = $(this).attr("offerId");
+        window.location.href = "/Home/Reservation?offerId=" + id + "&currency=" + $("[name='Currency']").val();
+    });
+
+    $("[name='btnBook']").click(function() {
+
+        var roomCount = $("[name='RoomCount']").val();
+
+        var resForm = new Object();
+        var transactionId = $("[name='TransactionId']").val();
+        resForm.TransactionId = transactionId;
+
+        var currency = $("[name='Currency']").val();
+        resForm.Currency = currency;
+
+        var agencyResNumber = $("[name='AgencyReservationNumber']").val();
+        resForm.AgencyReservationNumber = agencyResNumber;
+
+        var resInfo = $("[name='ReservationInfo']").val();
+        resForm.ReservationInfo = resInfo;
+
+        var customerInfo = $("[name='CustomerInfo']").val();
+        resForm.CustomerInfo = customerInfo;
+
+        var rooms = new Array();
+
+        for (var r = 1; r <= roomCount; r++) {
+            var travellerCount = $("[name='TravellerCount" + r + "']").val();
+            var room = new Object();
+            room.RoomNumber = r;
+            var travellers = new Array();
+
+            for (var t = 1; t <= travellerCount; t++) {
+                var traveller = new Object();
+                traveller.Title = $("[name='title" + r + t + "']").val();
+                traveller.Name = $("[name='Name" + r + t + "']").val();
+                traveller.Surname = $("[name='Surname" + r + t + "']").val();
+                traveller.Nationality = $("[name='Nationality" + r + t + "']").val();
+                traveller.Code = $("[name='Code" + r + t + "']").val();
+                traveller.PhoneNumber = $("[name='PhoneNumber" + r + t + "']").val();
+                traveller.PassportNo = $("[name='PassportNo" + r + t + "']").val();
+                traveller.ExpireDate = $("[name='ExpireDate" + r + t + "']").val();
+                traveller.IssueDate = $("[name='IssueDate" + r + t + "']").val();
+                traveller.IssueCountry = $("[name='IssueCountry" + r + t + "']").val();
+
+                travellers.push(traveller);
+            }
+            room.Travellers = travellers;
+            rooms.push(room);
+        }
+        resForm.Rooms = rooms;
+        if (resForm != null) {
+            $.ajax({
+                type: "POST",
+                url: "/Home/Booking",
+                data: JSON.stringify(resForm),
+                contentType: "application/json; charset=utf-8",
+                dataType: "html",
+                success: function(response) {
+                    $('#reservationResult').html(response);
+                },
+                failure: function(response) {
+                    alert(response.responseText);
+                },
+                error: function(response) {
+                    alert(response.responseText);
+                }
+            });
+        }
+    });
+});
